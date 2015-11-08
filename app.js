cca53eb289e620f0f3b8be6323ae24b3d044ca7f
Tony Gu <gql423@gmail.com>
2015-11-08 11:37:41 -0800

    Added Angular factory, routing including new state post pages

--- app.js
+++ app.js
@@ -1,22 +1,48 @@
-var app = angular.module('PlayerNews', []);
+var app = angular.module('PlayerNews', ['ui.router']);
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
 
+  $stateProvider
+    .state('home', {
+      url:'/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller:'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
+
+app.factory('posts', [function(){
+  var o = {
+    posts: [{title:'Hello', link:'', upvotes: 0}]
+  };
+  return o;
+}])
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.posts = [
-	{title: 'post 1', upvotes: 5},
-	{title: 'post 2', upvotes: 2},
-	{title: 'post 3', upvotes: 15},
-	{title: 'post 4', upvotes: 9},
-	{title: 'post 5', upvotes: 4}
-  ];
+'posts',
+function($scope, posts){
+  $scope.test = 'Hello world!';
+  $scope.posts = posts.posts;
 
   $scope.addPost = function() {
   	if(!$scope.title || $scope.title === '') { return; }
   	$scope.posts.push({
   	  title: $scope.title,
   	  link: $scope.link,
-  	  upvotes: 0
+  	  upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea, but everything is wrong.', upvotes: 0}
+      ]
   	});
   	$scope.title = '';
   	$scope.link = '';
@@ -25,4 +51,11 @@ function($scope){
   $scope.incrementUpvotes = function(post) {
   	post.upvotes += 1;
   }
+}])
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post=posts.posts[$stateParams.id]
 }]);
